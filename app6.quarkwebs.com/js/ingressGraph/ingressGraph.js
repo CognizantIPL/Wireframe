7dd0a65322f9a93660bcac05058a8ca5a704c59e
navinbosco <navinboscoz@gmail.com>
2013-10-09 03:19:29 +0530

    Update ingressGraph.js

--- app6.quarkwebs.com/js/ingressGraph/ingressGraph.js
+++ app6.quarkwebs.com/js/ingressGraph/ingressGraph.js
@@ -3,7 +3,7 @@
 $(document).ready(function(){
 	var liveData = [$.gchart.series('Temp', [], 'red', 'ffffff')]; 
 	 var liveAxis = $.gchart.axis('bottom', [], 'black');
-var liveAxisY = $.gchart.axis('left', ['20','30','40','50'],'black');
+var liveAxisY = $.gchart.axis('left', ['20','30','40','50','60','70'],'black');
 	function liveUpdate() { 
 		
 	    var rotate = function(arr) { 	
@@ -50,7 +50,7 @@ myYaxisArray.push(data1);
 } ;      
 function myLiveStream(jsnData){
                                 //alert('hui');
-                                var temp = jsnData.body[0].params.Temperature.value;
+                                var temp = jsnData.body[0].params.temperature.value;
 //alert('h12ui'+temp);
                                 var timestamp=jsnData.body[0].timestamp;
                                 liveData[0].data=myRotate(temp);
@@ -62,32 +62,70 @@ function myLiveStream(jsnData){
                                 $('#typedChart').gchart('option', {series: liveData, axes: [liveAxis,liveAxisY]});
                 }
 
-$('.startscreaming').live('click', function(){
-	$('#typedChart').gchart({type: 'line', maxValue: 40, 
+                $('#typedChart').gchart({type: 'line', maxValue: 70, 
 	    series: liveData, axes: [liveAxis], legend: 'right'});
-	 //setInterval(liveUpdate, 1000);
-	initialize({key: "6ef50172-05b4-4308-80af-8d6681f4733e"
-        , secret: "0b342390-c5b1-47c2-a72a-58bd97589635"
-        , aToken: "null" 
-        , apiHost: "foresight-api-721608207.us-east-1.elb.amazonaws.com"}, function(err, streams){
+
+initialize({key: "2d80af65-896a-4b6d-9a77-b939a309d402"
+           , secret: "3834f777-c66f-4afd-bcd6-aa2b64ad7fe6"
+           , aToken: "null" 
+           , apiHost: "foresight-demo-21591536.us-east-1.elb.amazonaws.com"}, function(err, streams){
 		  if(streams){
-		    streaming({id: "ipl_insurance::3::6::*", reconnect: true}, function(err, sData){
-if(!err)
-    {//$('#sample').append("<br>"+JSON.stringify(sData)+"</br>");
+		    streaming({id: "ipl_insurance::1::1::*", reconnect: true}, function(err, sData){
+		      if(!err){
+		      	//alert(JSON.stringify(sData));
 myLiveStream(sData);
-}
-		      else alert(JSON.stringify(err));
+		      }
+		       // $('#sample').append("<br>"+JSON.stringify(sData)+"</br>");
+		      else alert(JSON.stringify(err))
 		    });
-			}else $('#sample').append(JSON.stringify(err));
-});
-	
-});
+ 			}else $('#sample').append(JSON.stringify(err));
+
+
+	});
+
 //Stop
-					$('.stopstreaming').live('click', function() {
-						var id = $('.deviceid').val();
-						stopStreaming(id);
-						$('#typedChart').gchart('destroy');
-					});
+					
+
+					$('.showhistory').live('click', function(){
+			historyData({sid:'1', from:$('.startTime').val(), to:$('.endTime').val(), columns: "nodeid,lat,lng,temperature",constraint:""}, function(hdata){
+			     // $('#sample').append(JSON.stringify(hdata));
+var hisData = [$.gchart.series('Temp', [], 'red', 'ffffff')]; 
+	 
+var histYAxis = $.gchart.axis('left', ['20','30','40','50','60','70'],'black');
+var strTime=$('.startTime').val();
+var edTime=$('.endTime').val();
+var histXAxis = $.gchart.axis('bottom', [], 'black');
+
+			     //alert(JSON.stringify(hdata));
+	var hisArray=new Array();
+$.each(hdata.data, function(i, object) {
+    $.each(object, function(property, value) {
+    	if(property==3){
+    		hisArray.push(value);
+    	}
+       
+    });
+});
+
+hisData[0].data=hisArray;
+//histXAxis[0].labels=hisArray;
+//alert(hisArray);
+$('#hisChart').gchart({type: 'line', maxValue: 70, 
+	    series: hisData, axes: [histXAxis], legend: 'right'});
+			$('#hisChart').gchart('option', {series: hisData, axes: [histXAxis,histYAxis]});      
+			});
+});
+	function getMinute(unixTimestamp) {
+    var dt = new Date(unixTimestamp * 1000);
+
+    var hours = dt.getHours();
+    var minutes = dt.getMinutes();
+    var seconds = dt.getSeconds();
+    if (minutes < 10) 
+     minutes = '0' + minutes;
+
+ return minutes;
+}
 
 });
 
