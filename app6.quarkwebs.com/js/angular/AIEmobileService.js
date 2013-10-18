ab6993c41264604b41ce9762bb2623ef904e51d4
gayatridas <gayatri.das@gmail.com>
2013-10-18 19:50:31 -0400

    GD: adding all missed out files

---
+++ app6.quarkwebs.com/js/angular/AIEmobileService.js
@@ -0,0 +1,37 @@
+﻿'use strict';
+aie.factory('AIEMobileServices', function ($resource, $q, $exceptionHandler, $log) {
+    var resource = $resource('https://aiemobileservice.azure-mobile.net/tables/smart_truck_tracker/:id', { id: '@id' },
+            {
+                'update': {
+                    'method': 'PATCH'
+                } // adding an update function
+            });
+    return {
+        getTrackingData: function (id) {
+            console.log("getTrackingData called");
+            var deferred = $q.defer();
+            resource.get({ id: id },
+                function (event) {
+                    deferred.resolve(event);
+                },
+                function (response) {
+                    deferred.reject(response);
+                });
+
+            return deferred.promise;
+        },
+        getTrackingDataForAllTrucks: function () {
+            console.log("getTrackingDataForAllTrucks called");
+            var deferred = $q.defer();
+            resource.query(function (event) {
+                deferred.resolve(event);
+            },
+                function (response) {
+                    deferred.reject(response);
+                });
+
+            return deferred.promise;
+        }
+
+    };
+});
---
+++ app6.quarkwebs.com/js/angular/Controller.js
@@ -0,0 +1,33 @@
+﻿'use strict';
+
+aie.controller('AIETrucksController', function AIETrucksController($scope, $timeout, $log, AIEMobileServices) {
+    google.maps.visualRefresh = true;
+
+    var mapOptions = {
+        zoom: 4,
+        center: new google.maps.LatLng(41.5, -72.6),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"),
+      mapOptions);
+
+
+    AIEMobileServices.getTrackingDataForAllTrucks().then(function (result) {
+        for (var i = 0; i < result.length; i++) {
+            var latLng = new google.maps.LatLng(result[i].latitude, result[i].longitude);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: latLng,
+                title: 'AIE Truck',
+                animation: google.maps.Animation.DROP
+            });
+            var infowindow = new google.maps.InfoWindow({
+                content: result[i].truck_number
+            });
+            google.maps.event.addListener(marker, 'click', function () {
+                infowindow.open(map, marker);
+            });
+        };
+    });
+   
+});
