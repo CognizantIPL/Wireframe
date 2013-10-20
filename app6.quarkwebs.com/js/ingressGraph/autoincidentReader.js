64590e023c8bfce7a5cf935885bbfd43cb59d52e
Vikram V <reachvikramv@gmail.com>
2013-10-20 06:56:36 -0400

    Scene 2 crash report page full functionality

--- app6.quarkwebs.com/js/ingressGraph/autoincidentReader.js
+++ app6.quarkwebs.com/js/ingressGraph/autoincidentReader.js
@@ -14,62 +14,109 @@ function getDetails(){
   var userdetail = document.URL.split('user=')[1];
   var user = userdetail.split("&")[0];
   if(user=="insured"){
-    document.getElementById("complete").style.visibility="hidden";
-    document.getElementById("reject").style.visibility="hidden";
-  }else{
     document.getElementById("process").style.visibility="hidden";
+    document.getElementById("reject").style.visibility="hidden";
+    document.getElementById("tow_truck").style.visibility="hidden";
+    document.getElementById("truck_header").style.visibility="hidden";
+    document.getElementById("accordion_policy").style.visibility="hidden";
+document.getElementById("accordion_policy").style.height="0px";
+ 
+  }else{
+    document.getElementById("request").style.visibility="hidden";
     document.getElementById("cancel").style.visibility="hidden";
   }
   return vehicleNo;
 }
 
 function readArrayItems(jsonData2, vehicleNo){
+var latitude,longitude;
 for(i=0;i<jsonData2.length;++i){
   if(jsonData2[i].truck_number == vehicleNo){
    /* alert(jsonData2[i+1].id);
     alert(!(jsonData2[i].id<jsonData2[i+1].id));
     if(!(jsonData2[i].id<jsonData2[i+1].id))
     {*/
-    document.getElementById("veh_id").value = jsonData2[i].id;
+    if (document.getElementById("veh_id").value!=''){
+document.getElementById("veh_id").value = document.getElementById("veh_id").value+','+jsonData2[i].id;}
+else
+{document.getElementById("veh_id").value = jsonData2[i].id;}
+if (document.getElementById("channel").value!=''){
+document.getElementById("channel").value = document.getElementById("channel").value+','+jsonData2[i].channel;}
+else
+{document.getElementById("channel").value = jsonData2[i].channel;}
+ 
     document.getElementById("trucknumber").innerHTML = jsonData2[i].truck_number;
+latitude=jsonData2[i].latitude;
     document.getElementById("latitude").innerHTML = jsonData2[i].latitude;
+longitude=jsonData2[i].longitude;
     document.getElementById("longitude").innerHTML = jsonData2[i].longitude;
     document.getElementById("message").innerHTML = jsonData2[i].message;
     document.getElementById("speed").innerHTML = jsonData2[i].speed + " kmph";
     document.getElementById("address").innerHTML = jsonData2[i].address;
-  //alert(JSON.stringify(jsonData2[i]));
-/*}*/
   }
 }
-  //(jsonData2[49].activeIndicator)
+initializeMap(parseFloat(latitude),parseFloat(longitude));
 }
 function handleClaim(action){
-  var veh_id = document.getElementById("veh_id").value;
+  var primary_keys = document.getElementById("veh_id").value;
+  var channels = document.getElementById("channel").value;
+  var truck_number = document.URL.split('vehicleNo=')[1];
   var azureClient1 = new WindowsAzure.MobileServiceClient('https://aiemobileservice.azure-mobile.net/', 'NYuUVUztAwEXJQZxOFbppximTExpoh26');
-var equipTable= azureClient1.getTable('smart_truck_incident');
+  var equipTable= azureClient1.getTable('smart_truck_incident');
+// alert(channels);
+var ids = primary_keys.split(',');
+var channel_ids = channels.split(',');
+$.each(ids, function( index, value ) {
+doUpdates(equipTable,action,value,truck_number,channel_ids[index]);
+});
+if(action=="request"){
+alert('Claim AAJ0100 has been successfully created. An email has been sent with the claim information. Please use this for your reference.')
+}
 if(action=="process"){
-    equipTable.update({                
-                id: parseInt(veh_id),
-                activeIndicator: 1
-            });
-  }
-  if(action=="cancel"){
-    equipTable.update({                
-                id: parseInt(veh_id),
-                activeIndicator: 3
-            });
+alert('Claim AAJ0100 has sent for further processing.')
+}
+if(action=="reject"){
+alert('Claim AAJ0100 has been rejected. An email has been sent to LifeVac.')
+}
+if(action=="Cancel"){
+alert('This incident has been cancelled and all records of the same have been removed.')
+}
+document.getElementById("request").disabled=true;
+document.getElementById("cancel").disabled=true;
+document.getElementById("process").disabled=true;
+document.getElementById("reject").disabled=true;
+}
 
+function doUpdates(equipTable,action,primaryKey,trucknumber,channel_id)
+{
+if(action=="request"){
+equipTable.update({                
+id: parseInt(primaryKey),
+truck_number:trucknumber,
+activeIndicator: 1
+});
+ }
+ if(action=="cancel"){
+equipTable.update({                
+id: parseInt(primaryKey),
+activeIndicator: 3,
+truck_number:trucknumber
+ 
+});
+
+ }
+  if(action=="process"){
+ equipTable.update({                
+id: parseInt(primaryKey),
+activeIndicator: 2,
+truck_number:trucknumber
+});
   }
-   if(action=="complete"){
-      equipTable.update({                
-                id: parseInt(veh_id),
-                activeIndicator: 2
-            });
-   }
-   if(action=="reject"){
-      equipTable.update({                
-                id: parseInt(veh_id),
-                activeIndicator: 4
-            });
-   }
+  if(action=="reject"){
+ equipTable.update({                
+id: parseInt(primaryKey),
+activeIndicator: 4,
+truck_number:trucknumber
+});
+  }
 }
