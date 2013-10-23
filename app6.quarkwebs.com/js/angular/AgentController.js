2465adab03eb3ac27f84002c2690aafc3b86e0e1
gayatridas <gayatri.das@gmail.com>
2013-10-23 10:10:23 -0400

    Gd: handling the null condition in the AgentController

--- app6.quarkwebs.com/js/angular/AgentController.js
+++ app6.quarkwebs.com/js/angular/AgentController.js
@@ -20,12 +20,16 @@ agentApp.filter('unique', function () {
     return function (input, key) {
         var unique = {};
         var uniqueList = [];
-        for (var i = 0; i < input.length; i++) {
+        if (input)
+        {
+            for (var i = 0; i < input.length; i++) {
             if (typeof unique[input[i][key]] == "undefined") {
                 unique[input[i][key]] = "";
                 uniqueList.push(input[i]);
+             }
             }
         }
+      
         return uniqueList;
     };
 });
